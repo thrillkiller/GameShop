228ac98b7cc9f678a5d3e7afedbda7e12bc2a662
thrillkiller <norton95@wp.pl>
2018-05-23 22:19:15 +0200

    -Fixed produtct controller

--- src/main/java/com/example/GameShop/Controllers/ProductsController.java
+++ src/main/java/com/example/GameShop/Controllers/ProductsController.java
@@ -4,12 +4,15 @@ import com.example.GameShop.Models.Game;
 import com.example.GameShop.Models.Product;
 import com.example.GameShop.Repositories.ProductRepo;
 import com.example.GameShop.Services.SettingService;
+import jdk.nashorn.internal.objects.annotations.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("unused")
 @Controller
@@ -25,7 +28,7 @@ public class ProductsController {
     public String getProducts(Model model)
     {
         model.addAttribute("supervisor",settingService.isSupervisor());
-        model.addAttribute("products",productRepo.findAll());
+        model.addAttribute("products", getProductsList(productRepo.findAll()));
         model.addAttribute("product", new Product());
         return "products";
     }
@@ -42,8 +45,18 @@ public class ProductsController {
             productRepo.save(product);
         }
         model.addAttribute("supervisor",settingService.isSupervisor());
-        model.addAttribute("products",productRepo.findAll());
+        model.addAttribute("products", getProductsList(productRepo.findAll()));
         return "products";
+    }
+
+    //Returns only those products which aren't games
+    ArrayList<Product> getProductsList(Iterable<Product> all){
+        ArrayList<Product> list = new ArrayList<>();
+        for(Product p : productRepo.findAll()){
+            if(p.getClass() == Product.class && p.getClass() != Game.class)
+                list.add(p);
+        }
+        return list;
     }
 }
 
